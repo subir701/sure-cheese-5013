a98f08a2a7911eb19e1cdc182ff1959bf28d6d63
Rude00 <subiradhikari181@gmail.com>
2022-12-19 02:01:30 +0530

    day-5

--- js/login.js
+++ js/login.js
@@ -2,21 +2,62 @@ let button=document.getElementById("btn");
 let cred=document.getElementById("log");
 let cont=document.getElementById("if");
 let pass=document.getElementById("password");
-button.addEventListener("click",()=>{
-    
-    if(cred.value=="subiradhikari181@gmail.com"){
-        if(pass.value=="1234"){
-            cont.innerHTML=null;
-            alert("Log In Successful")
-        }else{
-            let p=document.createElement("p");
-            p.innerText="Check Your Password";
-            cont.append(p);
-        }
-    }else{
-        cont.innerHTML=null;
-        let p=document.createElement("p");
-            p.innerText="Check Your Email";
-            cont.append(p);
+let data=JSON.parse(localStorage.getItem("sigin"))||[];
+let form=document.getElementById("myForm");
+let logindata=JSON.parse(localStorage.getItem("sigin"))||[]
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let formData={
+        email:form.newcred.value,
+        password:form.newpass.value
     }
+    data.push(formData);
+    console.log(data);
+    localStorage.setItem("sigin",JSON.stringify(formData));
+    window.location.assign("index.html");
 })
+button.addEventListener("click",()=>{
+        if(cred.value=="admin@gmail.com"){
+            if(pass.value=="1234"){
+                cont.innerHTML=null;
+                window.location.assign("index.html")
+                alert("Log In Successful")
+            }else{
+                let p=document.createElement("p");
+                p.innerText="Check Your Password";
+                cont.append(p);
+            }
+        }else{
+            cont.innerHTML=null;
+            let p=document.createElement("p");
+                p.innerText="Check Your Email";
+                cont.append(p);
+        }
+
+})
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
---
+++ js/thriller.js
@@ -0,0 +1,25 @@
+let data=JSON.parse(localStorage.getItem("thriller"))||[];
+let cont=document.getElementById("container")
+console.log(data);
+display(data);
+function display(data){
+    data.forEach((element) => {
+        let divimg=document.createElement("div");
+        divimg.setAttribute("class","card");
+        let image=document.createElement("img");
+        image.setAttribute("src",element.image);
+        let divdes=document.createElement("div");
+        divdes.setAttribute("class","overlay");
+        let p=document.createElement("p");
+        p.innerText=element.name;
+        let button=document.createElement("button");
+        button.setAttribute("class","carbtn");
+        button.innerText="Watch";
+        let i=document.createElement("i");
+        i.setAttribute("class","fa-solid fa-share-nodes");
+        i.innerText="Share";
+        divdes.append(p,button,i);
+        divimg.append(image,divdes);
+        cont.append(divimg);
+    });
+}
